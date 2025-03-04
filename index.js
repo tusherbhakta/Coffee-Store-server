8a9dd0581b731cbd023ff89be355e321f05bcb1f
tusherbhakta <tusheranta@gmail.com>
2025-03-04 20:46:25 +0530

    add PUT endpoint to update coffee details

--- index.js
+++ index.js
@@ -54,6 +54,27 @@ async function run() {
         res.send(result);
     })
 
+    app.put('/newcoffee/:id', async(req, res)=>{
+        const id = req.params.id;
+        const filter = {_id: new ObjectId(id)};
+        const options = {upsert: true};
+
+        const updatedCoffee = req.body;
+        const coffee = {
+            $set: {
+                name: updatedCoffee.name,
+                quantity: updatedCoffee.quantity,
+                supplier: updatedCoffee.supplier,
+                taste: updatedCoffee.taste,
+                category: updatedCoffee.category,
+                details: updatedCoffee.details,
+                photo: updatedCoffee.photo
+            },
+        };
+        const result = await coffeeCollection.updateOne(filter, coffee, options);
+        res.send(result);
+    })
+
     app.delete('/newcoffee/:id', async(req, res)=>{
         const id = req.params.id;
         const query = {_id: new ObjectId(id)};
